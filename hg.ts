7968ca11d3ac64612418dca568a596f0fcca8207
vladislav <vladaksenov31073@gmail.com>
2024-01-31 17:50:02 +1000

    new

---
+++ hg.ts
@@ -0,0 +1,15 @@
+class ClassName {}
+class Point { x: number; y: number; }
+let point : Point= new Point();
+function  rt (){
+    point.x = 1;
+    point.y = 2;
+}
+rt()
+console.log(point)
+class Point2{width:number; height:number }
+let point2:  Point2=new Point2()
+function er(){
+    point2.height=1
+    point2.width=2
+}
